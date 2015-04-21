460c265ef0869e23e7ca98b4f7270c5e6f1d78c9
Briggsy16 <mattbriggs@outlook.com>
2015-04-21 20:03:56 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/misc.xml
    	.idea/modules.xml

--- Invent With Python/Testing.py
+++ Invent With Python/Testing.py
@@ -1,7 +1,17 @@
-def sillycase(string):
-  string_length = len(string)
-  new_string = string[:2].lower()
-  new_string += string[2:len(string)].upper()
-  return new_string
+# E.g. word_count("I am that I am") gets back a dictionary like:
+# {'i': 2, 'am': 2, 'that': 1}
+# Lowercase the string to make it easier.
+# Using .split() on the sentence will give you a list of words.
+# In a for loop of that list, you'll have a word that you can
+# check for inclusion in the dict (with "if word in dict"-style syntax).
+# Or add it to the dict with something like word_dict[word] = 1.
 
-print(sillycase('Matt'))
+
+def word_count(string):
+    word_dict = {}
+    my_list = string.split()
+    for word in my_list:
+        if word in dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
