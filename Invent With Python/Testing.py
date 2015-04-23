39ba414b2ea089a9b173746b18688aee29368697
Briggsy16 <mattbriggs@outlook.com>
2015-04-23 19:05:12 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/misc.xml
    	.idea/modules.xml

--- Invent With Python/Testing.py
+++ Invent With Python/Testing.py
@@ -1,17 +1,16 @@
-# E.g. word_count("I am that I am") gets back a dictionary like:
-# {'i': 2, 'am': 2, 'that': 1}
-# Lowercase the string to make it easier.
-# Using .split() on the sentence will give you a list of words.
-# In a for loop of that list, you'll have a word that you can
-# check for inclusion in the dict (with "if word in dict"-style syntax).
-# Or add it to the dict with something like word_dict[word] = 1.
+# combo(['swallow', 'snake', 'parrot'], 'abc')
+# Output:
+# [('swallow', 'a'), ('snake', 'b'), ('parrot', 'c')]
+# If you use list.append(), you'll want to pass it a tuple of new values.
+# Using enumerate() here can save you a variable or two.
 
+def combo(first, second):
+  count = 0
+  list = []
+  for item in first:
+    a = first[count], second[count]
+    list.append(a)
+    count += 1
+  return list
 
-def word_count(string):
-    word_dict = {}
-    my_list = string.split()
-    for word in my_list:
-        if word in dict:
-            word_dict[word] += 1
-        else:
-            word_dict[word] = 1
+print(combo(['swallow', 'snake', 'parrot'], 'abc'))
---
+++ Little Projects/Test.py
@@ -0,0 +1,13 @@
+dicts = [
+    {'name': 'Michelangelo',
+     'food': 'PIZZA'},
+    {'name': 'Garfield',
+     'food': 'lasanga'},
+    {'name': 'Walter',
+     'food': 'pancakes'},
+    {'name': 'Galactus',
+     'food': 'worlds'}
+]
+
+for i in dicts:
+    print(i)
